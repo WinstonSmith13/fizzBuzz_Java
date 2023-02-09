3bea2a3ff471e297fa6f2dfbdfd732c5b109bc56
WinstonSmith13 <krouk.medy@gmail.com>
2023-02-09 11:49:12 +0100

    feat: condition for fizzbuzz v1.

---
+++ src/main/java/org/example/FizzBuzz.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+        for(int i = 1; i <=100; i++ ){
+            if (i%15 == 0){
+                System.out.println(i +  " FizzBuzz");
+            }
+            else if (i%3 == 0){
+                System.out.println(i +  " Fizz");
+            }
+            else if  (i%5 == 0){
+                System.out.println(i + " Buzz");
+            }
+            else{System.out.println(i);}}
+    }
+}
